ec557729574c134dc8efb5dfe112d91d392dc1e6
Nick Young <nick.young@auckland.ac.nz>
2018-04-27 15:52:18 +1200

    bump unity version, add System.IO and System.Runtime dlls, attempt to load DicomDirectory

--- Assets/LoadDICOM.cs
+++ Assets/LoadDICOM.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using Dicom.Imaging;
 using System.IO;
 using System.Linq;
+using Dicom.Media;
+using Dicom;
 
 public class LoadDICOM : MonoBehaviour {
 
@@ -11,11 +13,19 @@ public class LoadDICOM : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		var dict = new DicomDictionary();
+		dict.Load(Application.dataPath + "/StreamingAssets/Dictionaries/DICOM Dictionary.xml", DicomDictionaryFormat.XML);
+		DicomDictionary.Default = dict;
 		var path = Application.dataPath + "/StreamingAssets/DICOM";
 		foreach (var directory in Directory.GetDirectories(path))
 		{
+			var dd = DicomDirectory.Open(directory + "/DICOMDIR");
+			Debug.Log(dd.ToString());
+			return;
+			Debug.Log("Loading directory " + directory);
 			foreach (var file in Directory.GetFiles(directory).Where(name => !name.EndsWith(".meta")))
 			{
+				Debug.Log("Loading file " + file);
 				var image = new DicomImage(file);
 				var ri = image.RenderImage();
 				var tex = new Texture2D(image.Height, image.Width);
