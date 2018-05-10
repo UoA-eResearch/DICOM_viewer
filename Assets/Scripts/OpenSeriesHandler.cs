955821482567955a88204335f9c992faf9176dcf
Nick Young <nick.young@auckland.ac.nz>
2018-05-10 12:03:08 +1200

    organise project into directories

---
+++ Assets/Scripts/OpenSeriesHandler.cs
@@ -0,0 +1,27 @@
+﻿using Dicom.Media;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenSeriesHandler : MonoBehaviour {
+
+	public DicomDirectoryRecord record;
+	public LoadDICOM loadDicomInstance;
+	private Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+		renderer = gameObject.GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SliderChange(float newValue)
+	{
+		var tex = loadDicomInstance.GetImageForRecord(record, (int)newValue);
+		renderer.material.mainTexture = tex;
+	}
+}
